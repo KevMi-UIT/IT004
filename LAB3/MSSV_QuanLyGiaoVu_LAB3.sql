33c5777d3a3bbb5bae878eb26e7949e25c485b43
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-11-01 16:27:01 +0700

    feat(lab3): 23, 24

--- LAB3/MSSV_QuanLyGiaoVu_LAB3.sql
+++ LAB3/MSSV_QuanLyGiaoVu_LAB3.sql
@@ -81,10 +81,40 @@ FROM
   INNER JOIN MHDAT ON MHKHONGDAT.MAMH = MHDAT.MAMH;
 
 -- 23. Tìm giáo viên (mã giáo viên, họ tên) là giáo viên chủ nhiệm của một lớp, đồng thời dạy cho lớp đó ít nhất một môn học.
+SELECT
+  MAGV,
+  HOTEN
+FROM
+  LOP
+  INNER JOIN GIAOVIEN ON LOP.MAGVCN = GIAOVIEN.MAGV
+WHERE
+  EXISTS (
+    SELECT
+      *
+    FROM
+      GIANGDAY
+    WHERE
+      GIAOVIEN.MAGV = GIANGDAY.MAGV
+  );
+
 -- 24. Tìm họ tên lớp trưởng của lớp có sỉ số cao nhất.
+SELECT
+  HO + ' ' + TEN AS HOTEN
+FROM
+  HOCVIEN
+  INNER JOIN LOP ON HOCVIEN.MALOP = LOP.MALOP
+WHERE
+  LOP.TRGLOP = HOCVIEN.MAHV
+  AND LOP.SISO = (
+    SELECT
+      MAX(SISO)
+    FROM
+      LOP
+  );
+
 -- 25. * Tìm họ tên những LOPTRG thi không đạt quá 3 môn (mỗi môn đều thi không đạt ở tất cả các lần thi).
--- 26. Tìm học viên (mã học viên, họ tên) có số môn đạt điểm 9,10 nhiều nhất.
--- 27. Trong từng lớp, tìm học viên (mã học viên, họ tên) có số môn đạt điểm 9,10 nhiều nhất.
+-- 26. Tìm học viên (mã học viên, họ tên) có số môn đạt điểm 9, 10 nhiều nhất.
+-- 27. Trong từng lớp, tìm học viên (mã học viên, họ tên) có số môn đạt điểm 9, 10 nhiều nhất.
 -- 28. Trong từng học kỳ của từng năm, mỗi giáo viên phân công dạy bao nhiêu môn học, bao nhiêu lớp.
 -- 29. Trong từng học kỳ của từng năm, tìm giáo viên (mã giáo viên, họ tên) giảng dạy nhiều nhất.
 -- 30. Tìm môn học (mã môn học, tên môn học) có nhiều học viên thi không đạt (ở lần thi thứ 1) nhất.
